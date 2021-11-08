8fb314a9c20518701b81c389beef4928fc76eedd
Dmitriy <dmitriyrodionov2015@mail.ru>
2021-11-08 16:33:08 +0300

    конвертация

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@ sek = document.querySelector('.sek-price');
 function getValues() {
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://www.cbr-xml-daily.ru/daily_json.js');
-// xhr.setRequestHeader('Content-type', 'application/json');
 xhr.send();
 
 xhr.addEventListener('load', () => {
@@ -37,32 +36,170 @@ xhr.addEventListener('load', () => {
       dkk.textContent = rate.Valute.DKK.Value;
       byn.textContent = rate.Valute.BYN.Value;
       sek.textContent = rate.Valute.SEK.Value;
-      console.log(rate);
-          //  usd = rate.Valute.USD.Value,
-          //  eur = rate.Valute.EUR.Value;
-      // console.log(rate.Date);
-      // console.log(`USD : ${usd}`);
-      // console.log(`EUR : ${eur}`);
   } else {
-      console.log('ERROR');
+      console.log('что-то не так');
   }
 });
 }
 getValues();
 
+
 //dropdown
 
-const menu = document.querySelector('.converter__first-menu'),
+const firstMenu = document.querySelector('.converter__first-menu'),
 firstCodeBtn = document.querySelector('.converter__first-code'),
-firstMenuItems = document.querySelectorAll('.converter__first-menu-item');
+firstMenuItems = document.querySelectorAll('.converter__first-menu-item'),
+secondMenu = document.querySelector('.converter__second-menu'),
+secondCodeBtn = document.querySelector('.converter__second-code'),
+secondMenuItems = document.querySelectorAll('.converter__second-menu-item');
 
 firstCodeBtn.addEventListener('click', () => {
-menu.classList.toggle('menu-active');
-});
+firstMenu.classList.toggle('menu-active');
+  });
 firstMenuItems.forEach(item => {
 item.addEventListener('click', () => {
   firstCodeBtn.textContent = item.textContent;
-  menu.classList.toggle('menu-active');
+  firstMenu.classList.toggle('menu-active');
+  setResultFirst();
+  });
 });
+secondCodeBtn.addEventListener('click', () => {
+secondMenu.classList.toggle('menu-active');
+});
+secondMenuItems.forEach(item => {
+item.addEventListener('click', () => {
+  secondCodeBtn.textContent = item.textContent;
+  secondMenu.classList.toggle('menu-active');
+  setResultFirst();
+  });
 });
 
+//converting
+
+const firstInput = document.querySelector('.converter__first-value'),
+secondInput = document.querySelector('.converter__second-value'),
+change = document.querySelector('.converter__change');
+
+let firstCurrency,
+secondCurrency;
+
+function setResultFirst() {
+  setCurrency();
+  let result = (+firstInput.value * firstCurrency) / secondCurrency;
+  secondInput.value = result.toFixed(3);
+  if(firstInput.value.length == 0) {
+      secondInput.value = '';
+  }
+}
+function setResultSecond() {
+  setCurrency();
+  let result = (+secondInput.value * secondCurrency) / firstCurrency;
+  firstInput.value = result.toFixed(3);
+  if(secondInput.value.length == 0) {
+      firstInput.value = '';
+  }
+}
+
+firstInput.addEventListener('input', setResultFirst);
+
+secondInput.addEventListener('input', setResultSecond);
+
+change.addEventListener('click', () => {
+  const u = firstCodeBtn.textContent,
+        r = secondCodeBtn.textContent;
+  firstCodeBtn.textContent = r;
+  secondCodeBtn.textContent = u;
+  setResultFirst();
+});
+
+function setCurrency() {
+switch(firstCodeBtn.textContent) {
+  case 'USD':
+      firstCurrency = +usd.textContent;
+      break;
+  case 'EUR':
+      firstCurrency = +eur.textContent;
+      break;
+  case 'GBP':
+      firstCurrency = +gbp.textContent;
+      break;
+  case 'CAD':
+      firstCurrency = +cad.textContent;
+      break;
+  case 'CHF':
+      firstCurrency = +chf.textContent;
+      break;
+  case 'CZK':
+      firstCurrency = +czk.textContent/10;
+      break;
+  case 'PLN':
+      firstCurrency = +pln.textContent;
+      break;
+  case 'NOK':
+      firstCurrency = +nok.textContent/10;
+      break;
+  case 'UAH':
+      firstCurrency = +uah.textContent/10;
+      break;
+  case 'HUF':
+      firstCurrency = +huf.textContent/100;
+      break;
+  case 'DKK':
+      firstCurrency = +dkk.textContent;
+      break;
+  case 'BYN':
+      firstCurrency = +byn.textContent;
+      break;
+  case 'SEK':
+      firstCurrency = +sek.textContent/10;
+      break;
+  case 'RUR':
+      firstCurrency = 1;
+      break;
+  }
+
+switch(secondCodeBtn.textContent) {
+  case 'USD':
+      secondCurrency = +usd.textContent;
+      break;
+  case 'EUR':
+      secondCurrency = +eur.textContent;
+      break;
+  case 'GBP':
+      secondCurrency = +gbp.textContent;
+      break;
+  case 'CAD':
+      secondCurrency = +cad.textContent;
+      break;
+  case 'CHF':
+      secondCurrency = +chf.textContent;
+      break;
+  case 'CZK':
+      secondCurrency = +czk.textContent/10;
+      break;
+  case 'PLN':
+      secondCurrency = +pln.textContent;
+      break;
+  case 'NOK':
+      secondCurrency = +nok.textContent/10;
+      break;
+  case 'UAH':
+      secondCurrency = +uah.textContent/10;
+      break;
+  case 'HUF':
+      secondCurrency = +huf.textContent/100;
+      break;
+  case 'DKK':
+      secondCurrency = +dkk.textContent;
+      break;
+  case 'BYN':
+      secondCurrency = +byn.textContent;
+      break;
+  case 'SEK':
+      secondCurrency = +sek.textContent/10;
+      break;
+  case 'RUR':
+      secondCurrency = 1;
+      break;
+  }
+}    
